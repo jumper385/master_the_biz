b8a8f9bd2c72907d0e1d810b378a4d3b1be17f50
Henry Chen <henrychen385@gmail.com>
2020-09-20 14:11:10 +0800

    added contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,27 @@
+<style>
+    .contact_block {
+        text-align: center;
+        box-sizing: border-box;
+    }
+</style>
+
+<h1 class='landing' style='background-image: linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.5)), url("https://images.unsplash.com/photo-1512626120412-faf41adb4874?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80");'>We'd love to talk</h1>
+<div style='padding: 12pt;'>
+    <h1 style='text-align:center; padding-top: 12pt; box-sizing:border-box; font-weight:600'>Let's get in Touch</h1>
+    <hr style='width:10vw; height:2pt; border:none; background:#28DF99; margin-bottom:24pt;'>
+    <div style='display:grid; grid-template-columns: 1fr 1fr 1fr; grid-column-gap:12pt'>
+        <div class='contact_block'>
+            <h2 class='contact_type'>Let's Meet Up!</h2>
+            <p class='contact_details'>42 Alison Way</p>
+            <p class='contact_details'>Palace Ave, WA 6084</p>
+        </div>
+        <div class='contact_block'>
+            <h2 class='contact_type'>Give us a Call?</h2>
+            <p class='contact_details'>+81 485 679</p>
+        </div>
+        <div class='contact_block'>
+            <h2 class='contact_type'>Or send us a Letter!</h2>
+            <p class='contact_details'>email@emailserver.net.au</p>
+        </div>
+    </div>
+</div>
