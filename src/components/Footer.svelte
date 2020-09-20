ea602687f0bd779ce01384e601b43fda913ed890
Henry Chen <henrychen385@gmail.com>
2020-09-20 14:10:03 +0800

    added img background to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,6 @@
-<div style='background:#28DF99; text-align:center; padding:12pt; box-sizing:border-box; font-weight:bold; color:white'>
-    © MasterTheBiz
+<div>
+    <div style='height:100pt; background:url("https://images.unsplash.com/photo-1600214051305-e908430c5eaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"); background-repeat: no-repeat; background-size: cover; background-position:center'></div>
+    <div style='background:#28DF99; text-align:center; padding:12pt; box-sizing:border-box; font-weight:bold; color:white'>
+        © MasterTheBiz
+    </div>
 </div>
